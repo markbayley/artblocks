27fb76dabad5a081c336b578132c7f926c83fa5d
Mark <markyb152@hotmail.com>
2023-06-14 10:07:50 +1000

    css and deply

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const INFURA_API_KEY = "e2337cf6fdc049598b4f70bf9662f984";
+
+const SEPOLIA_PRIVATE_KEY = "e89be75f7f6269191bf8db7d64698070258c3be0af6cc545580667c055e5a1d6";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
+
+
+
+
+
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,53 @@
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = "AI Generated NFT"
-  const SYMBOL = "AINFT"
-  const COST = ethers.utils.parseUnits("1", "ether") // 1 ETH
+  const NAME = "AI Generated NFT";
+  const SYMBOL = "AINFT";
+  const COST = ethers.utils.parseUnits("0.01", "ether");
 
-  const NFT = await hre.ethers.getContractFactory("NFT")
-  const nft = await NFT.deploy(NAME, SYMBOL, COST)
-  await nft.deployed()
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(NAME, SYMBOL, COST);
+  await nft.deployed();
 
-  console.log(`Deployed NFT Contract at: ${nft.address}`)
+  console.log(`Deployed NFT Contract at: ${nft.address}`);
+// Sepolia AI NFT contracted deployed at:
+// 0xe478889C4862D8892EcB7e8d10855de7539acb8B
+
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  // const [deployer] = await ethers.getSigners();
+
+  // console.log("Deploying contracts with the account:", deployer.address);
+
+  // const token = await ethers.deployContract("Token");
+
+  // console.log("Token address:", await token.getAddress());
+
+// const hre = require("hardhat");
+
+// async function main() {
+//   const NAME = "AI Generated NFT"
+//   const SYMBOL = "AINFT"
+//   const COST = ethers.utils.parseUnits("1", "ether") // 1 ETH
+
+//   const NFT = await hre.ethers.getContractFactory("NFT")
+//   const nft = await NFT.deploy(NAME, SYMBOL, COST)
+//   await nft.deployed()
+
+//   console.log(`Deployed NFT Contract at: ${nft.address}`)
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
